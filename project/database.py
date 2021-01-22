5200104182b21de8cce69a5137ce4e453830d882
Aleksi <paavola.aleksi@gmail.com>
2021-01-22 19:59:13 +0200

    test

--- project/database.py
+++ project/database.py
@@ -28,7 +28,7 @@ def config():
         # 'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
         # 'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DATABASE_NAME'),
+        'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),

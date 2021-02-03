16d5ec61f4b149b7072c74823a79518d8af535aa
Hilla Halla-aho <hilla.halla-aho@helsinki.fi>
2021-02-03 14:16:53 +0200

    django site id changed

--- project/settings.py
+++ project/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'behave_django',
 ]
 
-SITE_ID = 1
+SITE_ID = 2
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',

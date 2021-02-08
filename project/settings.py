903a54fb1b0e86b93f81b0d37e38f1a046fb1b1c
sainioan <sainio.anniina@gmail.com>
2021-02-08 08:22:08 +0200

    edit settings.py

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-import django_heroku
+
 import os
+import django.db.backends.mysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

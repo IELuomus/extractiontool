a305dfe5a1a48fb285da95096ba8fe566e6f9227
sainioan <sainio.anniina@gmail.com>
2021-02-09 09:21:19 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -147,7 +147,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DATABASE_NAME'),
-        'USER': 'root',
+        'USER': 'django',
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
         'PORT': os.getenv('DATABASE_PORT'),

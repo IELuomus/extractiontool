f230eac6d12daa0a4cfe17162adbf43b1eac24ba
sainioan <sainio.anniina@gmail.com>
2021-02-16 10:15:52 +0200

    html and requirements added

--- project/settings.py
+++ project/settings.py
@@ -198,6 +198,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+ACCOUNT_LOGOUT_ON_GET=False
+ACCOUNT_LOGOUT_REDIRECT_URL='/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 

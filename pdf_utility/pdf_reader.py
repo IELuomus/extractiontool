2c7f602d362c46d8fa6c16a155309df4295332ce
tnli <tnli@iki.fi>
2021-03-03 23:21:41 +0200

    cleaned up pdf_utility

---
+++ pdf_utility/pdf_reader.py
@@ -0,0 +1,112 @@
+import fitz
+import io
+import os
+import csv
+from pathlib import Path
+
+# test if the given path contains a file
+def path_to_file_has_file(path_to_file: Path):
+    return os.path.isfile(path_to_file)
+
+# read the text from the given pdf at the given path and output a txt-file
+def pdf_to_txt(pdf_name: str, path_to_file: Path):
+    if path_to_file_has_file(path_to_file): 
+        doc = fitz.open(path_to_file)
+        out = open(pdf_name + ".txt", "wb")
+        for page in doc:
+            text = page.getText().encode("utf8")
+            out.write(text)
+            out.write(bytes((12,)))
+        out.close()
+
+# read the text from the given pdf at the given path and output an xml
+def pdf_to_xml(pdf_name: str, path_to_file: Path):
+    if path_to_file_has_file(path_to_file): 
+        doc = fitz.open(path_to_file)
+        out = open(pdf_name + ".xml", "wb")
+        for page in doc:
+            text = page.getText("xml").encode("utf8")
+            out.write(text)
+            out.write(bytes((12,)))
+        out.close()
+
+# convert string-like list to list
+def stringList_to_list(stringList: str):
+    string = stringList.replace(')','').replace('(','')
+    output = string.replace("'",'').split(",")
+    return output
+ 
+# read text from a given pdf and outputs a list containing lists of each word.
+def pdf_to_words(pdf_name: str, path_to_file: Path):
+    if path_to_file_has_file(path_to_file):
+        doc = fitz.open(path_to_file)
+        pages = []
+        pagecounter = 1
+
+        for page in doc:
+            page_word_list = []
+            words_on_page = page.getText("words")
+
+            for word in words_on_page:
+                word_to_list = stringList_to_list(str(word))
+                word_to_list.append(pagecounter)
+                page_word_list.append(word_to_list)
+            
+            pages.append(page_word_list)
+            pagecounter += 1
+
+        return pages
+
+# Metadata getters
+def get_author(path_to_file: Path):
+    if path_to_file_has_file(path_to_file):
+        doc = fitz.open(path_to_file)
+        author = doc.metadata["author"]
+        return author
+
+def get_title(path_to_file: Path):
+    if path_to_file_has_file(path_to_file):
+        doc = fitz.open(path_to_file)
+        title = doc.metadata["title"]
+        return title
+
+def get_metadata(path_to_file: Path):
+    if path_to_file_has_file(path_to_file):
+        doc = fitz.open(path_to_file)
+        metadata = doc.metadata
+        return metadata
+
+# # Main for testing
+# if __name__ == "__main__":
+#     test_data_path = "../test_data/"
+#     source_directory = "Other"
+#     source = test_data_path + source_directory
+#     data_directory = Path(source)
+
+#     file_name = "mammalshb1.pdf"
+    
+#     path_to_file = data_directory / file_name
+
+#     pages = pdf_to_words(file_name, path_to_file)
+
+#     for page in pages:
+#         for word in page:
+#             print(f"{word}")
+
+    
+#     # files = []
+
+#     # for file in os.listdir(data_directory):
+#     #     path_to_file = data_directory / file
+        
+#     #     if path_to_file_has_file(path_to_file):
+#     #         files.append(path_to_file)
+
+#     # out = open("../test_data/results/" + source_directory + ".csv", "wb")
+#     # text = f"Filename; author; title; keywords; subject\n".encode("utf8")
+#     # out.write(text)
+#     # for file in files:
+#     #     metadata = get_metadata(file)
+#     #     text = f'{file.name}; {metadata["author"]}; {metadata["title"]}; {metadata["keywords"]}; {metadata["subject"]}\n'.encode("utf8")
+#     #     out.write(text)
+#     # out.close()

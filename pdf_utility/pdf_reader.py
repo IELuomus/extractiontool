c6bebe04fc09e821f2136e27cdd546d0f901a2bb
tnli <tnli@iki.fi>
2021-03-05 13:49:54 +0200

    wordlist works

--- pdf_utility/pdf_reader.py
+++ pdf_utility/pdf_reader.py
@@ -35,8 +35,25 @@ def stringList_to_list(stringList: str):
     string = stringList.replace(')','').replace('(','')
     output = string.replace("'",'').split(",")
     return output
- 
-# read text from a given pdf and outputs a list containing lists of each word.
+
+def str_is_int(test: str):
+    try:
+        int(test)
+        return True
+    except ValueError:
+        return False
+
+def str_is_float(test: str):
+    try:
+        float(test)
+        return True
+    except ValueError:
+        return False
+
+# Read text from a given pdf and outputs a list of pages containing a list with each word
+# on that page with the coordinates of the box that contains the word, the word itself, and 
+# additional information of the location of the word on the page, and the page number
+# STRIPS SOME OF THE PUNCTUATION!
 def pdf_to_words(pdf_name: str, path_to_file: Path):
     if path_to_file_has_file(path_to_file):
         doc = fitz.open(path_to_file)
@@ -50,6 +67,23 @@ def pdf_to_words(pdf_name: str, path_to_file: Path):
             for word in words_on_page:
                 word_to_list = stringList_to_list(str(word))
                 word_to_list.append(pagecounter)
+
+                # convert entries 0 to 3 into float, and 5 to 7 into int
+                for i in range(len(word_to_list)):
+                    if i >= 0 and i <= 3:
+                        if str_is_float(word_to_list[i]):
+                            converted_to_float = float(word_to_list[i])
+                            word_to_list[i] = converted_to_float
+                        else:
+                            word_to_list[i] = None
+
+                    if i >= 5 and i <= 7:
+                        if str_is_int(word_to_list[i]):
+                            converted_to_int = int(word_to_list[i])
+                            word_to_list[i] = converted_to_int
+                        else:
+                            word_to_list[i] = None
+
                 page_word_list.append(word_to_list)
             
             pages.append(page_word_list)
@@ -76,37 +110,37 @@ def get_metadata(path_to_file: Path):
         metadata = doc.metadata
         return metadata
 
-# # Main for testing
-# if __name__ == "__main__":
-#     test_data_path = "../test_data/"
-#     source_directory = "Other"
-#     source = test_data_path + source_directory
-#     data_directory = Path(source)
+# Main for testing
+if __name__ == "__main__":
+    test_data_path = "../test_data/"
+    source_directory = "Other"
+    source = test_data_path + source_directory
+    data_directory = Path(source)
 
-#     file_name = "mammalshb1.pdf"
+    file_name = "mammalshb1.pdf"
     
-#     path_to_file = data_directory / file_name
+    path_to_file = data_directory / file_name
 
-#     pages = pdf_to_words(file_name, path_to_file)
+    pages = pdf_to_words(file_name, path_to_file)
 
-#     for page in pages:
-#         for word in page:
-#             print(f"{word}")
+    for page in pages:
+        for word in page:
+            print(f"{word}")
 
     
-#     # files = []
+    # files = []
 
-#     # for file in os.listdir(data_directory):
-#     #     path_to_file = data_directory / file
+    # for file in os.listdir(data_directory):
+    #     path_to_file = data_directory / file
         
-#     #     if path_to_file_has_file(path_to_file):
-#     #         files.append(path_to_file)
+    #     if path_to_file_has_file(path_to_file):
+    #         files.append(path_to_file)
 
-#     # out = open("../test_data/results/" + source_directory + ".csv", "wb")
-#     # text = f"Filename; author; title; keywords; subject\n".encode("utf8")
-#     # out.write(text)
-#     # for file in files:
-#     #     metadata = get_metadata(file)
-#     #     text = f'{file.name}; {metadata["author"]}; {metadata["title"]}; {metadata["keywords"]}; {metadata["subject"]}\n'.encode("utf8")
-#     #     out.write(text)
-#     # out.close()
+    # out = open("../test_data/results/" + source_directory + ".csv", "wb")
+    # text = f"Filename; author; title; keywords; subject\n".encode("utf8")
+    # out.write(text)
+    # for file in files:
+    #     metadata = get_metadata(file)
+    #     text = f'{file.name}; {metadata["author"]}; {metadata["title"]}; {metadata["keywords"]}; {metadata["subject"]}\n'.encode("utf8")
+    #     out.write(text)
+    # out.close()

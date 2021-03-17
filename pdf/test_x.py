3f6c3315c94a867b2c6d529aefd4005302529c77
Jussi Asp <jussi.asp@helsinki.fi>
2021-03-17 05:36:02 +0200

    test a test

---
+++ pdf/test_x.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from pdf.models import Document
+import os
+
+# python3 manage.py test pdf.test_x
+
+# dummy test testing running test
+class PdfTestCase(TestCase):
+
+    def test_task_imagemajick(self):
+        """Pdf test x"""
+
+        message="HELLOU"
+        print(f'{message}!!!')
+
+        self.assertTrue(message == "HELLOU", f'Message is {message}.')

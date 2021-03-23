ec0d5574475838b1cf4b87dfc693f6e6c282ecf4
Anniina Sainio <sainio.anniina@gmail.com>
2021-03-23 11:30:34 +0200

    Update docker-entryprod.sh

--- docker-entryprod.sh
+++ docker-entryprod.sh
@@ -9,6 +9,8 @@ python manage.py makemigrations users
 python manage.py migrate users
 python manage.py migrate quality_control
 python manage.py migrate masterdata
+python manage.py migrate table
+python manage.py makemigrations table
 python manage.py makemigrations
 python manage.py migrate
 

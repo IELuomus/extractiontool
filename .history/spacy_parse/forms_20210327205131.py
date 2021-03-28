657c78a73c65a30dbac32e7679e2444742780236
sainioan <sainio.anniina@gmail.com>
2021-03-28 14:55:08 +0300

    ajax

---
+++ .history/spacy_parse/forms_20210327205131.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Pdf
+
+class TraitValuesForm(forms.Form):
+    traitvalues = forms.
+    class Meta:
+        fields = ['page_number']
+
---
+++ .history/spacy_parse/urls_20210328094350.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf.urls import url
+from project.views import health
+from django.urls.conf import include
+from spacy_parse.views import (
+  parse
+)
+
+urlpatterns = [
+  path('<int:pk>/', parse, name='parse'),
+  # path('<int:pk>/traitvalues/', traitvalues)
+  #path('parse/<int:pk>/', parse, name='parse'),
+  # url(r'^health$', health),
+  # path('upload/', include('project.urls')),
+    
+]

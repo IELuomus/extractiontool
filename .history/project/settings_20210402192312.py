8f5f13fa2e01c433e846d4e0b17175c4b1c1022c
sainioan <sainio.anniina@gmail.com>
2021-04-02 19:31:14 +0300

    fix favicon and fontawesome errors

--- .history/project/settings_20210402192312.py
+++ .history/project/settings_20210402192312.py
@@ -1,10 +1,10 @@
-from pathlib import Path
 import os
 import json
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()   
 
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'project',
     'pdf_utility',
     'django_userforeignkey',
-    'fontawesome-free',
+     'fontawesome-free',
     'spacy_parse',
     'table']
 
@@ -77,8 +77,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [os.path.join(BASE_DIR, "templates")],
-        'DIRS': [os.path.join(os.path.dirname(__file__), '../templates').replace('\\', '/')],
-
+        'DIRS': [ os.path.join(os.path.dirname(__file__) ,'../templates').replace('\\','/')],
+       
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +109,7 @@ ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
@@ -192,9 +192,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
 
-
 def show_toolbar(request):
     return True
-
-
 SHOW_TOOLBAR_CALLBACK = show_toolbar

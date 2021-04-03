71b00e3226a121c7ca42b668139f455b1838407c
sainioan <sainio.anniina@gmail.com>
2021-04-03 15:33:15 +0300

    check if pdf tables already exist

---
+++ .history/table/models_20210403083428.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+from django.db import models
+import jsonfield
+from django_mysql.models import JSONField
+from django.views.generic import ListView
+
+class Json_TableQuerySet(models.QuerySet):
+    
+    def delete(self, *args, **kwargs):
+        for obj in self:
+            obj.table.delete()
+        super(Json_TableQuerySet, self).delete(*args, **kwargs)
+
+
+class Json_Table(models.Model):
+    user_id = models.IntegerField(null=True)
+    pdf_id = models.IntegerField(null=True)
+    json_table= models.JSONField()
+    table = models.FileField(upload_to='json/')
+    
+    def delete(self, *args, **kwargs):
+        self.table.delete()
+        super(Json_Table, self).delete(*args, **kwargs)
+
+class TableListView(ListView):
+model = Json_Table
+template_name = 'tablepeople.html'
---
+++ .history/table/urls_20210403094224.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf.urls import url
+from project.views import health
+from django.urls.conf import include
+from .views import (
+  table_to_dataframe, redirect_form, json_table_list
+)
+
+
+urlpatterns = [
+  path('table/', table_to_dataframe, name="table"),
+  path('redirect_form/<int:pk>', redirect_form, name='redirect_form'),
+  path('selected_tables/<int:user_id>/<int:pdf_id>/', json_table_list, name='selected_tables'),
+  url(r'^health$', health),
+    
+]

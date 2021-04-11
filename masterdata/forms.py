c8dd906931e8c5f50acf9720c70a505c7233d8cb
Hilla Halla-aho <hilla.halla-aho@helsinki.fi>
2021-04-11 22:36:27 +0300

    cleanup & deleted quality_control & masterdata

--- masterdata/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class SourceDataImportForm(forms.Form):
-    source_name = forms.CharField(max_length=255)
-    file = forms.FileField(label="Upload a CSV file containing source data")
--- quality_control/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QualityControlConfig(AppConfig):
-    name = 'quality_control'

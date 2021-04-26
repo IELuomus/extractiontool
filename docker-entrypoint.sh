818802c2dde7eb0f8b048b3275726c18273a394f
johanneslares <Johq.lares@gmail.com>
2021-04-26 12:08:47 +0300

    Added document migrations to docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,8 @@ python manage.py makemigrations users
 python manage.py migrate users
 python manage.py makemigrations ner_trainer
 python manage.py migrate ner_trainer
+python manage.py makemigrations document
+python manage.py migrate document
 python manage.py migrate table
 python manage.py makemigrations table
 python manage.py makemigrations

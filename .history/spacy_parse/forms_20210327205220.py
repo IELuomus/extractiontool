193946012bec8d812d627935fcbd87c0b88f1a2b
Anniina Sainio <sainio.anniina@gmail.com>
2021-04-30 16:18:48 +0300

    Delete .history directory

--- .history/spacy_parse/forms_20210327205220.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.contrib.postgres.forms import SimpleArrayField
-
-class TraitValuesForm(forms.Form):
-    traitvalues = forms.SimpleArrayField()
-    class Meta:
-        fields = ['page_number']
-

579d652d060fefc67bbc8d4e9c2c85f082ba13bd
Jussi Asp <jussi.asp@helsinki.fi>
2021-05-01 22:20:48 +0300

    fixes, cleanup, tasks-related code.

--- devscripts/run_server.sh
+++ devscripts/run_server.sh
@@ -13,13 +13,13 @@ sudo /etc/init.d/mysql start
 
 echo
 cyan "running django-q cluster in the background."
-echo "python manage.py qcluster > log_qcluster.txt 2>&1 &" # runs django-q cluster
+echo "python manage.py qcluster > logs/log_qcluster.txt 2>&1 &" # runs django-q cluster
 python manage.py qcluster > logs/log_djangoq_cluster.txt 2>&1 &
 task_id_djangoq_cluster=$!
 
 echo
 cyan "running django server."
-echo "python manage.py runsslserver 2>&1 | tee log_django_server.txt" # runs the webserver
+echo "python manage.py runsslserver | tee logs/log_django_server.txt 2>&1" # runs the webserver
 python manage.py runsslserver 2>&1 | tee logs/log_django_server.txt
 echo "django server stopped."
 

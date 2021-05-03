fae8101f5637d4e86afaa0c840ea67f9e8c8da3f
Jussi Asp <jussi.asp@helsinki.fi>
2021-05-03 21:40:29 +0300

    new comment.

--- devscripts/windows/run_windows_docker.bat
+++ devscripts/windows/run_windows_docker.bat
@@ -1,7 +1,7 @@
 @REM windows .bat 
 
 @REM USE:
-@REM with Command Prompt: devscripts\windows\run_windows_docker.bat
+@REM with PowerShell or Command Prompt: devscripts\windows\run_windows_docker.bat
 
 docker-compose -f docker-compose-dev.yml build 
 docker-compose -f docker-compose-dev.yml up 

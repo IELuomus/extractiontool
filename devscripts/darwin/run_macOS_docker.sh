8536c6702bdde2169077acff434a2aa432c36cc4
Jussi Asp <jussi.asp@helsinki.fi>
2021-05-10 11:26:10 +0300

    docker start scripts polishing.

--- devscripts/darwin/run_macOS_docker.sh
+++ devscripts/darwin/run_macOS_docker.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Linux
-
 # run like so:
 # have .env in extractiontool/ root ( check devscripts/.env-file-example )
-# bash devscripts/run_docker.sh
+# bash devscripts/darwin/run_macOS_docker.sh
 
 # docker complains if not set
 export HOST=''
 
-sudo /etc/init.d/mysql stop
+sudo mysql.server stop
 
 sudo docker-compose -f docker-compose-dev.yml build 
 sudo docker-compose -f docker-compose-dev.yml up 
--- devscripts/windows/run_windows_docker.bat
+++ devscripts/windows/run_windows_docker.bat
@@ -1,6 +1,7 @@
 @REM windows .bat 
 
 @REM USE:
+@REM have .env in extractiontool/ root ( check devscripts/.env-file-example )
 @REM with PowerShell or Command Prompt: devscripts\windows\run_windows_docker.bat
 
 docker-compose -f docker-compose-dev.yml build 
